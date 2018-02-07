43f54629f0b104a4dfe5b5756cf263f9afaf5adc
Florian Egermann <florian@macht-medien.de>
2018-02-07 21:50:10 +0100

    make the settings page stay on the same tab after saving

--- admin/assets/js/settings.js
+++ admin/assets/js/settings.js
@@ -1,7 +1,12 @@
 (function($) {
   'use strict';
   $(function() {
-    $('#tabs').tabs();
+		$('#tabs').tabs({
+			beforeActivate: function (event, ui) {
+				var hash = ui.newTab.children("li a").attr("href");
+				window.location.hash = hash;
+			}
+		});
   });
 // Place your administration-specific JavaScript here
 })(jQuery);
