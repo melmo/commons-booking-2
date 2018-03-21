43c982f233e8822683e98991b507925a356ae6b7
Florian Egermann <florian@macht-medien.de>
2018-03-21 12:51:08 +0100

    fix: js error because no variables  (yet)

--- public/assets/js/public.js
+++ public/assets/js/public.js
@@ -2,7 +2,8 @@
   'use strict';
   $(function() {
     // Write in console log the PHP value passed in enqueue_js_vars in public/class-plugin-name.php
-    console.log(pn_js_vars.alert);
+		// console.log(pn_js_vars.alert);
+
   });
 // Place your public-facing JavaScript here
 })(jQuery);
